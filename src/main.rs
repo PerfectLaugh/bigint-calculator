466a1c555bb2cbde63988e30542ef0a2bfba5611
PerfectLaugh <denniswu81229@gmail.com>
2022-09-02 01:06:49 +0800

    use lalrpop

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,30 @@
+use std::collections::HashMap;
+
 #[macro_use]
-extern crate pest_derive;
+extern crate lalrpop_util;
 
-use num_bigint::BigInt;
+lalrpop_mod!(grammar);
 
-use pest::Parser;
-use pest::iterators::Pair;
-
-enum AST {
-    Ident(String),
-    Number(BigInt),
-    Add(Box<AST>, Box<AST>),
-    Sub(Box<AST>, Box<AST>),
-    Mul(Box<AST>, Box<AST>),
-    Div(Box<AST>, Box<AST>),
-    Mod(Box<AST>, Box<AST>),
-    Pow(Box<AST>, Box<AST>),
-}
-
-#[derive(Parser)]
-#[grammar = "grammar.pest"]
-struct ProgParser;
-
-pub struct Calculator {
-}
-
-impl Calculator {
-    pub fn new() -> Calculator {
-        Calculator {}
-    }
-
-    pub fn calculate(&self, input: &str) -> Result<BigInt, String> {
-        let pairs = ProgParser::parse(Rule::prog, input)
-            .map_err(|e| format!("Error parsing input: {}", e))?;
-
-        let mut result = BigInt::from(0);
-        for pair in pairs {
-            match pair.as_rule() {
-                Rule::number => {
-                    result = pair.as_str().parse::<BigInt>().unwrap();
-                }
-                Rule::add => {
-                    result += pair.as_str().parse::<BigInt>().unwrap();
-                }
-                Rule::sub => {
-                    result -= pair.as_str().parse::<BigInt>().unwrap();
-                }
-                Rule::mul => {
-                    result *= pair.as_str().parse::<BigInt>().unwrap();
-                }
-                Rule::div => {
-                    result /= pair.as_str().parse::<BigInt>().unwrap();
-                }
-                _ => unreachable!(),
-            }
-        }
-
-        Ok(result)
-    }
-
-    fn visit_term<'s>(&self, pair: Pair<'s, Rule>, mut operand: BigInt) -> BigInt {
-        let rule = pair.as_rule();
-        assert_eq!(rule, Rule::term);
-        let inner = pair.into_inner();
-
-        for pair in inner {
-            match pair.as_rule() {
-                Rule::term => {
-                    operand = self.visit_term(pair, operand);
-                }
-                Rule::add => {
-
-                }
-                _ => unimplemented!()
-            }
-        }
-        match inner {
-            Rule::term => {
-                self.visit_term(, num)
-            }
-            _ => unimplemented!(),
-        }
-    }
+#[derive(Debug)]
+pub enum CalcError {
+    UnknownVariable(String),
+    PowTooLarge,
 }
 
 fn main() {
-    let res = ProgParser::parse(Rule::prog, "(t * 20) + 30");
+    let mut state = HashMap::new();
+    grammar::StmtParser::new()
+        .parse(
+            &mut state,
+            "let a = 2201381908479023740917509813750817085337801471083271098371083712;",
+        )
+        .unwrap();
+    let res = grammar::StmtParser::new()
+        .parse(
+            &mut state,
+            "(a + 31490871094829048190438129038901839012839021809320 * 101329013810938920831) ^ 2;",
+        )
+        .unwrap();
     println!("{:?}", res);
 }
 
@@ -93,6 +33,5 @@ mod test {
     //use super::*;
 
     #[test]
-    fn test_parse() {
-    }
+    fn test_parse() {}
 }
